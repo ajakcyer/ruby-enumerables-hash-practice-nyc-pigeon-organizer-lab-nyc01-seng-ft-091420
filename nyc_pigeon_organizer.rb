e798034c4be4fa376fd936b4a718f8cee37100c3
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 23:15:53 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,43 @@
+require 'pry'
+data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each do |cgl, values|
+    values.each do |value, array|
+      array.each do |names|
+        if new_hash[names] == nil
+          new_hash[names] = {}
+        end
+        
+        if new_hash[names][cgl] == nil
+          new_hash[names][cgl] = []
+        end
+        new_hash[names][cgl].push(value.to_s)
+      end
+    end
+  end
+  
+  new_hash
+  #binding.pry
 end
+
+nyc_pigeon_organizer(data)
