ee6a33a38991a5657e5b02a36dc623ca7ef88961
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 22:10:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  names_array = []
+  names_hash = {}
+  
+  data.collect do |cgl, values|
+    values.collect do |cgl_att, pigeon_arr|
+      pigeon_arr.collect do |names|
+        names_array.push(names)
+        # binding.pry
+      end
+      names_array.uniq!
+    end
+  end
+  # binding.pry
+  # binding.pry
 end
