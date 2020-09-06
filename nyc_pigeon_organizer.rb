d4107146fd2922eb2039277b9befd7ab88529bd1
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 22:17:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,6 +2,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |color_gender_lives, values|
+    binding.pry
     puts new_hash[color_gender_lives] = values
     binding.pry
   end
