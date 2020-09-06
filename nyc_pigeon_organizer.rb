dc5bcd8240efd1728a07f0528b0ec14481424a0f
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-06 22:22:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,8 @@ def nyc_pigeon_organizer(data)
     color_gender_lives.each do |value, array|
       array.each do |name|
         if new_hash[name] == nil
-          new_hash[name] = {}binding.pry
+          new_hash[name] = {}
+          binding.pry
           new_hash[name][color_gender_lives] = []
           
       end
